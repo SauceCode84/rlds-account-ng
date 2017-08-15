54d5c7555cba0940b22c3b547b62772d37ffabb3
Richard Ueckermann <rueckermann@sse.co.za>
2017-08-15 11:45:49 +0200

    created Statement service

---
+++ src/providers/statement.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+import { Statement } from "models";
+
+@Injectable()
+export class StatementService {
+
+  constructor() { }
+
+  getStatement(): Statement {
+
+  }
+
+}
