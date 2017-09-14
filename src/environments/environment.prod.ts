0d3772e0331234a559eb0a0e3ea6e532c5dbdd2f
SauceCode84 <rayruok@gmail.com>
2017-09-14 10:45:18 +0200

    deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBXMFZoCp-oGr9JZzBwDME1_vUAMAeK5Ls",
+    authDomain: "rlds-invoice.firebaseapp.com",
+    databaseURL: "https://rlds-invoice.firebaseio.com",
+    projectId: "rlds-invoice",
+    storageBucket: "rlds-invoice.appspot.com",
+    messagingSenderId: "61007094691"
+  }
 };
