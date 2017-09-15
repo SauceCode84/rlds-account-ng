9b5513ff87ed8a0cec46bbd9fd71c0eefcf92990
SauceCode84 <rayruok@gmail.com>
2017-09-15 09:14:59 +0200

    wiring up login form

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+import { AuthService } from "providers/auth.service";
 
 @Component({
   selector: "login-form",
@@ -7,9 +10,30 @@ import { Component, OnInit } from "@angular/core";
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  loading: boolean = false;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
+    this.loginForm = this.buildForm();
+  }
+
+  private buildForm() {
+    return this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  async onLogin() {
+    if (this.loginForm.valid) {
+      let { email, password } = this.loginForm.value;
+
+      this.loading = true;
+      await this.authService.emailLogin(email, password);
+      this.loading = false;
+    }
   }
 
 }
