8c824631d57cda191765f228af1c55d3a6d7d37e
SauceCode84 <rayruok@gmail.com>
2017-10-27 16:45:23 +0200

    updated environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyBXMFZoCp-oGr9JZzBwDME1_vUAMAeK5Ls",
-    authDomain: "rlds-invoice.firebaseapp.com",
-    databaseURL: "https://rlds-invoice.firebaseio.com",
-    projectId: "rlds-invoice",
-    storageBucket: "rlds-invoice.appspot.com",
-    messagingSenderId: "61007094691"
-  }
+  apiUrl: "http://188.166.153.17/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,5 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBXMFZoCp-oGr9JZzBwDME1_vUAMAeK5Ls",
-    authDomain: "rlds-invoice.firebaseapp.com",
-    databaseURL: "https://rlds-invoice.firebaseio.com",
-    projectId: "rlds-invoice",
-    storageBucket: "rlds-invoice.appspot.com",
-    messagingSenderId: "61007094691"
-  },
   apiUrl: "http://localhost:3000"
 };
