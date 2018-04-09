959f8feecd9845cb77921789618d40f37a7aaa30
SauceCode84 <rayruok@gmail.com>
2018-04-09 12:01:04 +0200

    fixed config import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
   AlertService,
   //AngularFireKeyService,
   AuthService,
+  ConfigService,
   FeesService,
   NotificationService,
   StatementService,
@@ -52,7 +53,6 @@ import { FormatNumberDirective } from "../directives/format-number.directive";
 import { environment } from "environments/environment";
 import { SummaryService } from "providers/summary.service";
 import { SummaryChartComponent } from "./summary-chart/summary-chart.component";
-import { ConfigService } from "providers/config.service";
 
 @NgModule({
   declarations: [
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -2,6 +2,7 @@
 export { AlertService } from "./alert.service";
 //export { AngularFireKeyService } from "./angular-fire-key.service";
 export { AuthService } from "./auth.service";
+export { ConfigService } from "./config.service";
 export { FeesService } from "./fees.service";
 export { GradesService } from "./grades.service";
 export { NotificationService } from "./notification.service";
